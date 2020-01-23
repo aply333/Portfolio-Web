9bef0c99d5a8a563fa034f9104feec16e55f9ec4
Andrei Lysenko <andrei.lysenko@outlook.com>
2020-01-22 23:46:19 -0800

    setting up an array for projects

---
+++ js/dataFiles/projects.js
@@ -0,0 +1,6 @@
+const project =[
+    {projectName:'Restaurant Passport',
+        projectImg:"./img/resPas.png",
+        link:"https://bw-restaurant-passport-1.github.io/User-Interface/",
+        gitHub:"https://github.com/bw-restaurant-passport-1/User-Interface"}
+]
